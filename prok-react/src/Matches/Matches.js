8608e953afcfc3e3d0b00ce073d9c2500738f48e
Anthony McDonald <a@awews.com>
2019-07-17 19:04:39 -0700

    Revert "Revert "  add socket.io-client and enable live scores""
    This reverts commit 4d73a020900abd27d3954f87768392881e741548.

--- prok-react/src/Matches/Matches.js
+++ prok-react/src/Matches/Matches.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import openSocket from "socket.io-client";
 import Match from "./Match/Match";
 
+const API_URL = "http://localhost:8080";
+
 const API_URL_MATCHES = "/matches";
+const API_URL_TEAMS = "/teams";
+const socket = openSocket(API_URL);
 
 const Matches = () => {
   const [matches, setMatches] = useState([]);
   const [showAllMatches, setShowAllMatches] = useState(true);
+  const [teams, setTeams] = useState([]);
   const [awayScore, setAwayScore] = useState(0);
   const [homeScore, setHomeScore] = useState(0);
   const [balls, setBalls] = useState(0);
@@ -14,11 +20,12 @@ const Matches = () => {
   const [fouls, setFouls] = useState(0);
   const [outs, setOuts] = useState(0);
 
-  const incrementScore = function() {
-    // console.log("setHomeScore", setHomeScore);
-    setHomeScore(homeScore + 1);
+  const getMatches = async function() {
+    const res = await axios(API_URL_MATCHES);
+    if (showAllMatches) {
+      setMatches(res.data);
+    }
   };
-  // console.log("incrementScore", incrementScore);
 
   let currentMatches = null;
 
@@ -38,15 +45,22 @@ const Matches = () => {
   if (matches.length > 0) {
     // console.log("matches.length", matches.length);
     currentMatches = matches.map(match => {
+      const awayTeamObj = teams.find(team => {
+        return match.awayTeam === team._id;
+      });
+      const homeTeamObj = teams.find(team => {
+        return match.homeTeam === team._id;
+      });
       return (
         <Match
           key={match._id}
           matchId={match._id}
           match={match}
+          awayTeamObj={awayTeamObj}
           awayScore={awayScore}
           setAwayScore={setAwayScore}
+          homeTeamObj={homeTeamObj}
           homeScore={homeScore}
-          incrementScore={incrementScore}
           balls={balls}
           setBalls={setBalls}
           strikes={strikes}
@@ -63,18 +77,30 @@ const Matches = () => {
   }
 
   useEffect(() => {
-    async function getMatches() {
-      const res = await axios(API_URL_MATCHES);
-      if (showAllMatches) {
-        setMatches(res.data);
-      }
-    }
     getMatches();
   }, [showAllMatches]);
 
   useEffect(() => {
     document.title = `${matches.length} matches currently`;
   }, [matches, showAllMatches]);
+
+  useEffect(() => {
+    socket.on("createdMatch", data => {
+      console.log("data", data);
+      if (data.action === "createdMatch") {
+        getMatches();
+      }
+    });
+  });
+
+  useEffect(() => {
+    socket.on("updatedMatch", data => {
+      console.log("data", data);
+      if (data.action === "updatedMatch") {
+        getMatches();
+      }
+    });
+  }, []);
 
   return (
     <div className="matches">
